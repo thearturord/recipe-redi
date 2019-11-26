40436dbaea0312e06bf1a3ac6df943e1b33d987b
thearturord <47672781+thearturord@users.noreply.github.com>
2019-11-26 19:06:27 +0100

    a

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+
+
+
+
+
+let api = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?";
+let i = "i=";
+let q = "q=";
+let p = "p=";
+
+// let econdedField = encodeURIComponent(ingredients);
+
 function onSearchClicked() {
     let ingredients = document.getElementById("search").value;
     console.log(ingredients);
@@ -5,38 +17,36 @@ function onSearchClicked() {
 }
 
 async function getRecipes(ingredients) {
-    console.log(ingredients);
-    let response = await fetch("https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/");
+    let response = await fetch(api + i + ingredients);
     let recipeListObj = await response.json();
     let recipeList = recipeListObj.results;
-    console.log(recipeList);
-    
-    let recipeListWrapper = document.createElement('div');
-    recipeListWrapper.id = 'recipeList';
-    document.body.appendChild(recipeListWrapper);
-
-    for (let i = 0; i < recipeList.length; i++) {
-        let recipeEl = document.createElement('div');
-        recipeEl.className = 'recipeEl';
-        recipeListWrapper.appendChild(recipeEl);
-
-        let recipeImgEl = document.createElement('div');
-        recipeImgEl.className = 'thumbnailImg';
-        recipeEl.appendChild(recipeImgEl);
-
-        let thumbnailImg = document.createElement('img');
-        thumbnailImg.src = recipeList[i].thumbnail;
-        recipeImgEl.appendChild(thumbnailImg);
-
-        let recipeInfoEl = document.createElement('div');
-        recipeInfoEl.className = 'recipeInfo';
-        recipeEl.appendChild(recipeInfoEl);
-
-        let recipeName = document.createElement('h2');
-        recipeName.className = 'recipeName';
-        recipeName.textContent = recipeList[i].title;
-        recipeInfoEl.appendChild(recipeName);
-    }
-    return recipeList;
+    console.log(recipeListObj);
+    //
+    // let recipeListWrapper = document.createElement('div');
+    // recipeListWrapper.id = 'recipeList';
+    // document.body.appendChild(recipeListWrapper);
+    //
+    // for (let i = 0; i < recipeList.length; i++) {
+    //     let recipeEl = document.createElement('div');
+    //     recipeEl.className = 'recipeEl';
+    //     recipeListWrapper.appendChild(recipeEl);
+    //
+    //     let recipeImgEl = document.createElement('div');
+    //     recipeImgEl.className = 'thumbnailImg';
+    //     recipeEl.appendChild(recipeImgEl);
+    //
+    //     let thumbnailImg = document.createElement('img');
+    //     thumbnailImg.src = recipeList[i].thumbnail;
+    //     recipeImgEl.appendChild(thumbnailImg);
+    //
+    //     let recipeInfoEl = document.createElement('div');
+    //     recipeInfoEl.className = 'recipeInfo';
+    //     recipeEl.appendChild(recipeInfoEl);
+    //
+    //     let recipeName = document.createElement('h2');
+    //     recipeName.className = 'recipeName';
+    //     recipeName.textContent = recipeList[i].title;
+    //     recipeInfoEl.appendChild(recipeName);
+    // }
+    // return recipeList;
 }
-
