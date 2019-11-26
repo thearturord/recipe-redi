7ea28b2b4296380fba2713272b25c7a24545a960
katamatata <51786805+katamatata@users.noreply.github.com>
2019-11-26 14:42:25 +0100

    using spoonacular API

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+let apiKey = "90b4bf234e22493e89325b6f9cce868e";
+
+let baseUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey;
+
 function onSearchClicked() {
-    let ingredients = document.getElementById("search").value;
-    console.log(ingredients);
-    getRecipes(ingredients);
+    let ingredients = document.getElementById("searchField").value;
+    // console.log(ingredients);
+    let results = getRecipes(ingredients);
+    // console.log(results);
+    showResults(results);
 }
 
 async function getRecipes(ingredients) {
-    console.log(ingredients);
-    let response = await fetch("https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/");
-    let recipeListObj = await response.json();
-    let recipeList = recipeListObj.results;
-    console.log(recipeList);
-    
+    let url = baseUrl + "&ingredients=" + encodeURIComponent(ingredients) + "&number=2";
+    // console.log(url);
+    let response = await fetch(url);
+    let recipeList = await response.json();
+    // console.log(recipeList);
+    return recipeList;
+}
+
+function showResults(recipeList) {
     let recipeListWrapper = document.createElement('div');
     recipeListWrapper.id = 'recipeList';
     document.body.appendChild(recipeListWrapper);
+    console.log(recipeList);
 
     for (let i = 0; i < recipeList.length; i++) {
         let recipeEl = document.createElement('div');
@@ -25,7 +35,7 @@ async function getRecipes(ingredients) {
         recipeEl.appendChild(recipeImgEl);
 
         let thumbnailImg = document.createElement('img');
-        thumbnailImg.src = recipeList[i].thumbnail;
+        thumbnailImg.src = recipeList[i].image;
         recipeImgEl.appendChild(thumbnailImg);
 
         let recipeInfoEl = document.createElement('div');
@@ -37,6 +47,4 @@ async function getRecipes(ingredients) {
         recipeName.textContent = recipeList[i].title;
         recipeInfoEl.appendChild(recipeName);
     }
-    return recipeList;
 }
-
